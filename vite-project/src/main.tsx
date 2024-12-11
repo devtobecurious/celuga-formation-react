6aac8bc3a6da340cea6adb2434b201ad215117b4
Evan BOISSONNOT <evan@boissonnot.fr>
2024-12-11 10:31:24 +0100

    feat: main ts amélioré, package avec npm start

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+
+if (!root) { // typeguard
+  throw new Error(
+    "Avez-vous pensé à ajouter l'objet avec l'id root dans votre html ?"
+  );
+}
+
+// typeguard : vu que j'ai controlé avant si null, et bien ici, tsc reconnait que pas null
+createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
